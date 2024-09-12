81e14d6149b9313273c51157122a4aaa51cf36e6
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-12 09:49:19 -0300

    Teste modulo data

--- 2a-vpc_app/outputs.tf
+++ 2a-vpc_app/outputs.tf
@@ -1,4 +1,4 @@
-output "vpc" {
-  description = "App VPC information"
-  value       = module.vpc_app.vpc
-}
+# output "vpc" {
+#   description = "App VPC information"
+#   value       = module.vpc_app.vpc
+# }
