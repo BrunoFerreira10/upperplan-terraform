a383ade798daaf82e5e5cdc9afc9d08ad31b4d52
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-12 10:44:46 -0300

    Adicionado shortname

--- 6a-elb_app/data.tf
+++ 6a-elb_app/data.tf
@@ -1,5 +1,6 @@
 module "data" {
   source = "../modules/data"
+  shortname = var.general_tag_shortname
   requested_data = [
     "vpc_app",
     "acm_app",
--- 8a-deploy_app/data.tf
+++ 8a-deploy_app/data.tf
@@ -1,5 +1,6 @@
 module "data" {
   source = "../modules/data"
+  shortname = var.general_tag_shortname
   requested_data = [
     "rds_app",
     "elb_app"
