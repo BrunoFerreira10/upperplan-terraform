3fbebe8aae698f3a02595cecabb6184e673438ba
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-13 11:08:44 -0300

    Adicionadas politicas do lambda e sns

--- modules/compute/ec2_image_builder/generic_container_image_builder/01_iam.tf
+++ modules/compute/ec2_image_builder/generic_container_image_builder/01_iam.tf
@@ -92,8 +92,87 @@ resource "aws_iam_role_policy_attachment" "ecr_push_to_codebuild" {
   role       = aws_iam_role.codebuild.name
 }
 
+## --------------------------------------------------------------------------------------------------------------------
+## Lambda policies and role
+## --------------------------------------------------------------------------------------------------------------------
+resource "aws_iam_policy" "lambda_codedeploy_policy" {
+  name        = "LambdaCodeDeployPolicy-${var.shortname}-${var.region}"
+  path        = "/TerraformManaged/"
+  description = "Policy to allow Lambda function to trigger CodeDeploy for ${var.shortname}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "codedeploy:GetDeploymentConfig",
+          "codedeploy:GetApplicationRevision",
+          "codedeploy:CreateDeployment",
+          "codedeploy:RegisterApplicationRevision"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Resource": "arn:aws:s3:::${var.project_bucket_name}/*"
+      }
+    ]
+  })
+}
+
+## Lamda role
+resource "aws_iam_role" "lambda_codedeploy_role" {
+  name = "LambdaCodeDeployRole-${var.shortname}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_codedeploy_attach_policy" {
+  role       = aws_iam_role.lambda_codedeploy_role.name
+  policy_arn = aws_iam_policy.lambda_codedeploy_policy.arn
+}
+
+## --------------------------------------------------------------------------------------------------------------------
+## SNS 
+## --------------------------------------------------------------------------------------------------------------------
+# Documento de política IAM para permitir que o S3 publique mensagens no SNS
+data "aws_iam_policy_document" "topic" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["SNS:Publish"]
+    resources = [aws_sns_topic.s3_deployment_notifications.arn]  # Corrigido para referenciar o tópico SNS criado
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [data.aws_s3_bucket.project_bucket_name.arn]  # Corrigido para usar o ARN do bucket S3 referenciado
+    }
+  }
+}
+
 ## ---------------------------------------------------------------------------------------------------------------------
-## Codebuild ECR Push Role
+## Codedeploy ECS Deploy Role
 ## ---------------------------------------------------------------------------------------------------------------------
 
 ## -- Policies ---------------------------------------------------------------------------------------------------------
