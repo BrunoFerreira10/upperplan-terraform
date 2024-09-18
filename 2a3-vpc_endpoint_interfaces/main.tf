0b2c140a6fd574a728346ba18c68b088a29a24cd
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-18 11:54:56 -0300

    Deasativados vpc endpoints

--- 2a3-vpc_endpoint_interfaces/main.tf
+++ 2a3-vpc_endpoint_interfaces/main.tf
@@ -14,50 +14,67 @@
 #   vpc       = module.data.projects.vpc.vpc
 # }
 
-module "ecr_api_vpc_endpoint" {
-  source = "../modules/networking/vpc/generic_vpc_endpoint_interface"
-  region = module.data.github_vars.general_region
-  service_name_sufix = "ecr.api"
-  sg_vpc_endpoint_interface_rules = {
-    ingress = {
-      All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
-    },
-    egress = {
-      All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
-    }
-  }
-  shortname = module.data.github_vars.general_tag_shortname
-  vpc       = module.data.projects.vpc.vpc
-}
+## ---------------------------------------------------------------------------------------------------------------------
+## VPC Endpoint Interface - ECR API
+## Dependentes
+## - ECS Task Start - Faz o pull do ECR Registry que depende desse endpoint
+## ---------------------------------------------------------------------------------------------------------------------
+# module "ecr_api_vpc_endpoint" {
+#   source = "../modules/networking/vpc/generic_vpc_endpoint_interface"
+#   region = module.data.github_vars.general_region
+#   service_name_sufix = "ecr.api"
+#   sg_vpc_endpoint_interface_rules = {
+#     ingress = {
+#       All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
+#     },
+#     egress = {
+#       All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
+#     }
+#   }
+#   shortname = module.data.github_vars.general_tag_shortname
+#   vpc       = module.data.projects.vpc.vpc
+# }
 
-module "ecr_dkr_vpc_endpoint" {
-  source = "../modules/networking/vpc/generic_vpc_endpoint_interface"
-  region = module.data.github_vars.general_region
-  service_name_sufix = "ecr.dkr"
-  sg_vpc_endpoint_interface_rules = {
-    ingress = {
-      All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
-    },
-    egress = {
-      All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
-    }
-  }
-  shortname = module.data.github_vars.general_tag_shortname
-  vpc       = module.data.projects.vpc.vpc
-}
+## ---------------------------------------------------------------------------------------------------------------------
+## VPC Endpoint Interface - ECR DKR (Docker)
+## Dependentes
+## - ECS Task Start - Faz o pull do ECR Registry que depende desse endpoint
+## ---------------------------------------------------------------------------------------------------------------------
+# module "ecr_dkr_vpc_endpoint" {
+#   source = "../modules/networking/vpc/generic_vpc_endpoint_interface"
+#   region = module.data.github_vars.general_region
+#   service_name_sufix = "ecr.dkr"
+#   sg_vpc_endpoint_interface_rules = {
+#     ingress = {
+#       All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
+#     },
+#     egress = {
+#       All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
+#     }
+#   }
+#   shortname = module.data.github_vars.general_tag_shortname
+#   vpc       = module.data.projects.vpc.vpc
+# }
 
-module "logs_vpc_endpoint" {
-  source = "../modules/networking/vpc/generic_vpc_endpoint_interface"
-  region = module.data.github_vars.general_region
-  service_name_sufix = "logs"
-  sg_vpc_endpoint_interface_rules = {
-    ingress = {
-      All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
-    },
-    egress = {
-      All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
-    }
-  }
-  shortname = module.data.github_vars.general_tag_shortname
-  vpc       = module.data.projects.vpc.vpc
-}
+## ---------------------------------------------------------------------------------------------------------------------
+## VPC Endpoint Interface - ClooudWatch Logs
+## Dependentes
+## - ECS Task - Precisa enviar logs para o CloudWatch
+## ---------------------------------------------------------------------------------------------------------------------
+# module "logs_vpc_endpoint" {
+#   source = "../modules/networking/vpc/generic_vpc_endpoint_interface"
+#   region = module.data.github_vars.general_region
+#   service_name_sufix = "logs"
+#   sg_vpc_endpoint_interface_rules = {
+#     ingress = {
+#       All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
+#     },
+#     egress = {
+#       All = { ip_protocol = "-1", cidr_ipv4 = "0.0.0.0/0" }
+#     }
+#   }
+#   shortname = module.data.github_vars.general_tag_shortname
+#   vpc       = module.data.projects.vpc.vpc
+# }
+
+## ---------------------------------------------------------------------------------------------------------------------
