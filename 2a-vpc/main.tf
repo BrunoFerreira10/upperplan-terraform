b31709dbf7ab56323acd89c8e028c805c7c1776c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-22 11:24:13 -0300

    Adicionado SMTP

--- 2a-vpc/main.tf
+++ 2a-vpc/main.tf
@@ -7,31 +7,32 @@ module "vpc_app" {
     nacl_rules = {
       public = {
         ingress = {
-          SSH       = { rule_number = 100, cidr_block = "${data.aws_ssm_parameter.my_ip.value}/32", port = 22 },
-          HTTP      = { rule_number = 200, cidr_block = "0.0.0.0/0", port = 80 },
-          HTTPS     = { rule_number = 300, cidr_block = "0.0.0.0/0", port = 443 },
-          EPHEMERAL = { rule_number = 400, cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
+          SSH       = { rule_number = 150, cidr_block = "${data.aws_ssm_parameter.my_ip.value}/32", port = 22 },
+          HTTP      = { rule_number = 250, cidr_block = "0.0.0.0/0", port = 80 },
+          HTTPS     = { rule_number = 350, cidr_block = "0.0.0.0/0", port = 443 },
+          SMTP      = { rule_number = 450, cidr_block = "0.0.0.0/0", port = 587 },
+          EPHEMERAL = { rule_number = 9999, cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
         },
         egress = {
-          SSH_GIT   = { rule_number = 50, port = 22, cidr_block = "0.0.0.0/0" }, # GIT IMAGE BUILDER
-          SSH       = { rule_number = 100, port = 22 },
-          HTTP      = { rule_number = 200, cidr_block = "0.0.0.0/0", port = 80 },
-          HTTPS     = { rule_number = 300, cidr_block = "0.0.0.0/0", port = 443 },
-          EPHEMERAL = { rule_number = 500, cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
+          SSH       = { rule_number = 150, port = 22 },
+          HTTP      = { rule_number = 250, cidr_block = "0.0.0.0/0", port = 80 },
+          HTTPS     = { rule_number = 350, cidr_block = "0.0.0.0/0", port = 443 },
+          SMTP      = { rule_number = 450, cidr_block = "0.0.0.0/0", port = 587 },
+          EPHEMERAL = { rule_number = 9999, cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
         }
       },
       private = {
         ingress = {
-          SSH            = { rule_number = 100, port = 22 },
-          HTTP           = { rule_number = 200, port = 80 },
-          EPHEMERAL_TEMP = { rule_number = 400, from_port = 1024, to_port = 65535, cidr_block = "0.0.0.0/0" } # USER DATA
-          EPHEMERAL      = { rule_number = 500, from_port = 1024, to_port = 65535 }
+          SSH            = { rule_number = 150, port = 22 },
+          HTTP           = { rule_number = 250, port = 80 },
+          EPHEMERAL      = { rule_number = 9999, from_port = 1024, to_port = 65535, cidr_block = "0.0.0.0/0" }
         },
         egress = {
-          SSH       = { rule_number = 102, port = 22, cidr_block = "0.0.0.0/0" },  # USER DATA
-          HTTPS     = { rule_number = 202, port = 443, cidr_block = "0.0.0.0/0" }, # USER DATA
-          HTTP      = { rule_number = 302, port = 80, cidr_block = "0.0.0.0/0" },  # USER DATA
-          EPHEMERAL = { rule_number = 402, from_port = 1024, to_port = 65535 }
+          SSH       = { rule_number = 150, port = 22, cidr_block = "0.0.0.0/0" },  # USER DATA
+          HTTPS     = { rule_number = 250, port = 443, cidr_block = "0.0.0.0/0" }, # USER DATA
+          HTTP      = { rule_number = 350, port = 80, cidr_block = "0.0.0.0/0" },  # USER DATA
+          SMTP      = { rule_number = 450, cidr_block = "0.0.0.0/0", port = 587 },
+          EPHEMERAL = { rule_number = 9999, from_port = 1024, to_port = 65535 }
         }
       }
     }
